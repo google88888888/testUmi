897ccee507fc5b49daf11fe3f68380c908edaeb6
孟久翔 <mengjx@sq108.com>
2020-03-05 10:10:16 +0800

    添加model

---
+++ src/pages/user/model.tsx
@@ -0,0 +1,18 @@
+export default {
+    namespace: 'user',
+    state: {
+        show: "userShow",
+    },
+    reducers: {
+        save(state, action) {
+            return { ...state, ...action.payload }
+        }
+    },
+    effects: {
+        * getTopOrderDetail(action, { call, put, select }) {
+            const { OrderNo } = yield select(state => state.user);
+        },
+    },
+    subscriptions: {
+    }
+}
